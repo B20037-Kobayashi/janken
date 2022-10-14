49d08fcb74d15b0727bf8cd1ff1ec7b7e56c5ed3
B20037-Kobayashi <e1b20037@oit.ac.jp>
2022-10-14 18:57:06 +0900

    DIコンテナを利用したログインの実装

--- src/main/java/oit/is/z0340/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0340/kaizi/janken/controller/JankenController.java
@@ -1,38 +1,37 @@
 package oit.is.z0340.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z0340.kaizi.janken.model.*;
 
-/*
-@ControllerをつけているからHTTPリクエストがあったら，このクラスがよばれる
-*/
-
 @Controller
+@RequestMapping("/janken")
 public class JankenController {
-  /**
-   * jankenというGETリクエストがあったら janken()を呼び出し，janken.htmlを返す
-   */
 
-  @GetMapping("/janken")
-  public String janken() {
+  @Autowired
+  public Room room;
+
+  @GetMapping("entry")
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
     return "janken.html";
   }
 
-  @PostMapping("/janken")
-  public String formJanken(@RequestParam String userName, ModelMap model) {
-    model.addAttribute("userName", userName);
-    return "janken.html";
-  }
-
-  @GetMapping("/janken/{hand}")
-  public String janken(@PathVariable Integer hand, ModelMap model) {
-    Janken judge = new Janken(hand);
+  @GetMapping("botbattle/{UHNum}")
+  public String janken(@PathVariable Integer UHNum, ModelMap model) {
+    Janken judge = new Janken(UHNum);
+    judge.BotBattle();
     model.addAttribute("judge", judge);
     return "janken.html";
   }
